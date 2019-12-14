1b0f1a2cca13f4ecf6b0b9219b81a4385757988f
Jonathan <lokkhj@gmail.com>
2019-12-14 00:20:24 -0500

    Fixed some bugs and optimized some code - Fixed bug where you could spam a channel twice and get the bot to spam twice as fast - Fixed bug where the bot sends the stop info twice in the same channel -Fixed bug where the bot allows you to stop a stopped punishment/channel treatment - Removed unused function "Spam()" - Removed unused variable "spamTimer" - Removed unused variable "_spamChannel" - Stop spam now uses channel name instead of id - Override punish now uses infringer name instead of id - Existing punishments stack up when another mod calls another punish command

--- Don't Spam Spammer/Don't spam spammer.cs
+++ Don't Spam Spammer/Don't spam spammer.cs
@@ -8,20 +8,11 @@ namespace Discord_Bot_Template
 {
     public static class DontSpamSpammer
     {
-        private static IMessageChannel _spamChannel;
-        static Timer spamTimer = new Timer(10000);
         public static List<Timer> spamTimers = new List<Timer>();
         public static Dictionary<int, Timer> userPunishmentLengths = new Dictionary<int, Timer>();
-        public static Dictionary<int, IMessageChannel> treatmentIdChannelPairs = new Dictionary<int, IMessageChannel>();
-        public static Dictionary<int, IUser> punishmentIdUserPairs = new Dictionary<int, IUser>();
-
-
-        public static void Spam(IMessageChannel spamChannel, SocketGuild guild)
-        {
-            _spamChannel = spamChannel;
-            spamTimer.Elapsed += delegate (object sender, ElapsedEventArgs e) { SpamChannelTimer_Elapsed(sender, e, spamChannel); };
-            spamTimer.Start();
-        }
+        public static Dictionary<int, System.Diagnostics.Stopwatch> punishmentIdElapsedTimePairs = new Dictionary<int, System.Diagnostics.Stopwatch>();
+        public static Dictionary<IMessageChannel, int> channelTreatmentIdPairs = new Dictionary<IMessageChannel, int>();
+        public static Dictionary<IUser, int> userPunishmentIdPairs = new Dictionary<IUser, int>();
 
         public static void SpamChannel(IMessageChannel spamChannel)
         {
@@ -29,7 +20,7 @@ namespace Discord_Bot_Template
 
             int timerNumber = spamTimers.Count - 1;
 
-            treatmentIdChannelPairs.Add(timerNumber, spamChannel);
+            channelTreatmentIdPairs.Add(spamChannel, timerNumber);
 
             spamTimers[timerNumber].Elapsed += delegate (object sender, ElapsedEventArgs e) { SpamChannelTimer_Elapsed(sender, e, spamChannel); };
 
@@ -42,23 +33,56 @@ namespace Discord_Bot_Template
             spamTimers.Add(new Timer(10000));
             int timerNumber = spamTimers.Count - 1;
             //set up time up timer
+            int userTimerNumber;
+            if (userPunishmentIdPairs.TryGetValue(infringer, out int oldId))
+            {
+                //modify the user id to match the new one
+                userPunishmentIdPairs[infringer] = timerNumber;
+            }
+            else
+            {
+                //link the id to the user
+                userPunishmentIdPairs.Add(infringer, timerNumber);
+            }
+            //create new timer for user
             userPunishmentLengths.Add(timerNumber, new Timer(length * 1000));
-            int userTimerNumber = userPunishmentLengths.Count - 1;
+            //set reference number
+            userTimerNumber = userPunishmentLengths.Count - 1;
+            //turn off auto-reset
             userPunishmentLengths[timerNumber].AutoReset = false;
-            //link the id to the user
-            punishmentIdUserPairs.Add(timerNumber, infringer);
-            //finish and start spam timer
+            //subscribe to events
             spamTimers[timerNumber].Elapsed += delegate (object sender, ElapsedEventArgs e) { SpamUserTimer_Elapsed(sender, e, infringer); };
+            userPunishmentLengths[timerNumber].Elapsed += delegate (object sender, ElapsedEventArgs e) { SpamUserStop(timerNumber, infringer, $"your {punishmentIdElapsedTimePairs[timerNumber].ElapsedMilliseconds / 1000} seconds is up!"); };
+            //setup and start stopwatch
+            punishmentIdElapsedTimePairs.Add(timerNumber, new System.Diagnostics.Stopwatch());
+            punishmentIdElapsedTimePairs[timerNumber].Start();
+            //start spam timer
             spamTimers[timerNumber].Start();
-            //finish and start time up timer
-            userPunishmentLengths[timerNumber].Elapsed += delegate (object sender, ElapsedEventArgs e) { SpamUserStop(timerNumber, infringer, $"your {userPunishmentLengths[timerNumber].Interval / 1000} seconds is up!"); };
+            //start time up timer
             userPunishmentLengths[timerNumber].Start();
+        }
+
+        public static void StackSpamUser(IUser infringer, ulong timeIncreased)
+        {
+            //get id
+            int id = userPunishmentIdPairs[infringer];
+            //modify time up timer
+            userPunishmentLengths[id].Interval = timeIncreased * 1000 + (userPunishmentLengths[id].Interval - punishmentIdElapsedTimePairs[id].ElapsedMilliseconds);
         }
 
         public static void StopSpam(int id)
         {
-            //stop timer
             spamTimers[id].Stop();
+        }
+
+        public static Embed ViewSpam()
+        {
+            EmbedBuilder instancesEmbedBuilder = new EmbedBuilder
+            {
+
+            };
+
+            return instancesEmbedBuilder.Build();
         }
 
         private static void SpamChannelTimer_Elapsed(object sender, ElapsedEventArgs e, IMessageChannel spamChannel)
@@ -75,10 +99,15 @@ namespace Discord_Bot_Template
             //note the infringer
             infringer.SendMessageAsync($"The anti-spam treatment is now over, {reason}");
 
+            //stop stopwatch
+            punishmentIdElapsedTimePairs[id].Stop();
+
+            //stop spamming
             StopSpam(id);
 
             //get end timer
             userPunishmentLengths.TryGetValue(id, out Timer timer);
+
             if (reason.Contains("stopped the treatment!"))
             {
                 //if mod stopped stop the timers manually
